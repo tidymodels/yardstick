dd70ba552caa4c38c8717f65a3872f238655ee2d
Max Kuhn <mxkuhn@gmail.com>
2017-10-31 13:36:06 -0400

    Somer's J

--- R/two_class.R
+++ R/two_class.R
@@ -2,7 +2,7 @@
 #' 
 #' General metrics for two class problems that are not already
 #'  in [sens()] or [recall()] are here, such as the Matthews
-#'  correlation coefficient
+#'  correlation coefficient, Youden's J
 #' 
 #' @inheritParams sens
 #' @author Max Kuhn
@@ -49,5 +49,34 @@ mcc.data.frame  <-
     if (d1 == 0 | d2 == 0 | d3 == 0 | d4 == 0)
       return(NA)
     ((tp * tn) - (fp * fn)) / sqrt(d1 * d2 * d3 * d4)
+  }
+
+#' @export 
+som_j <- function(data, ...)
+  UseMethod("som_j")
+
+#' @export
+#' @rdname mcc
+som_j.data.frame  <-
+  function(data, truth = NULL, estimate = NULL, na.rm = TRUE, ...) {
+    check_call_vars(match.call(expand.dots = TRUE))
+    xtab <- vec2table(
+      truth = get_col(data, truth),
+      estimate = get_col(data, estimate),
+      na.rm = na.rm,
+      two_class = TRUE,
+      dnn = c("Prediction", "Truth"),
+      ...
+    )
+    som_j.table(xtab, ...)
+  }
+
+#' @rdname mcc
+#' @export
+"som_j.table" <-
+  function(data, ...) {
+    ## "truth" in columns, predictions in rows
+    check_table(data)
+    sens(data) + spec(data) - 1
   }
 
