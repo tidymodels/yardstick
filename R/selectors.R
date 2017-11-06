e6ebd0e8bc4ce79effa50540d0caacfb1185352a
Max Kuhn <mxkuhn@gmail.com>
2017-11-06 12:19:02 -0500

    Laying the groundwork for issue #1 solution

---
+++ R/selectors.R
@@ -0,0 +1,69 @@
+
+#' @importFrom rlang exiting abort
+abort_selection <- rlang::exiting(function(cnd) {
+  rlang::abort("No variables or terms were selected.")
+})
+
+#' @importFrom rlang with_handlers enquo quos
+#' @importFrom tidyselect vars_select vars_pull
+
+prob_select <- function(data, truth, ...) {
+  truth_var <- tidyselect::vars_pull(names(data), !! enquo(truth))
+  dot_vars <- rlang::with_handlers(
+    tidyselect::vars_select(names(data), !!! quos(...)),
+    tidyselect_empty = abort_selection
+  )
+  if (length(dot_vars) == 0) {
+    stop("No class probability columns were selected by the `...`.",
+         call. = FALSE)
+  }
+  if(!is.factor(data[[truth_var]]))
+    stop("`", truth_var, "` should be a factor.", call. = FALSE)
+  prob_num <- vapply(data[, dot_vars, drop = FALSE], 
+                     is.numeric, logical(1))
+  if(any(!prob_num))
+    stop("The columns selected for class probabiltiies should ",
+         "be numeric: ", 
+         paste0("`", names(prob_num)[!prob_num], "`", collapse = ", "),
+         call. = FALSE)
+  
+  list(truth = truth_var, probs = unname(dot_vars))
+}
+
+both_select <- function(data, truth, estimate, ...) {
+  truth_var <- tidyselect::vars_pull(names(data), !! enquo(truth))
+  est_var <- tidyselect::vars_pull(names(data), !! enquo(estimate))
+  dot_vars <- rlang::with_handlers(
+    tidyselect::vars_select(names(data), !!! quos(...)),
+    tidyselect_empty = abort_selection
+  )
+  if (length(dot_vars) == 0) {
+    stop("No class probability columns were selected by the `...`.",
+         call. = FALSE)
+  }
+  if(!is.factor(data[[truth_var]]))
+    stop("`", truth_var, "` should be a factor.", call. = FALSE)
+  if(!is.factor(data[[est_var]]))
+    stop("`", est_var, "` should be a factor.", call. = FALSE)
+  prob_num <- vapply(data[, dot_vars, drop = FALSE], 
+                     is.numeric, logical(1))
+  if(any(!prob_num))
+    stop("The columns selected for class probabiltiies should ",
+         "be numeric: ", 
+         paste0("`", names(prob_num)[!prob_num], "`", collapse = ", "),
+         call. = FALSE)
+  
+  list(truth = truth_var, estimate = est_var, probs = unname(dot_vars))
+}
+
+
+factor_select <- function(data, truth, estimate, ...) {
+  truth_var <- tidyselect::vars_pull(names(data), !! enquo(truth))
+  est_var <- tidyselect::vars_pull(names(data), !! enquo(estimate))
+  if(!is.factor(data[[truth_var]]))
+    stop("`", truth_var, "` should be a factor.", call. = FALSE)
+  if(!is.factor(data[[est_var]]))
+    stop("`", est_var, "` should be a factor.", call. = FALSE)
+  
+  list(truth = truth_var, estimate = est_var)
+}
---
+++ tests/testthat/test_selectors.R
@@ -0,0 +1,130 @@
+library(testthat)
+library(yardstick)
+library(tidyselect)
+
+###################################################################
+
+test_that('just factors', {
+  expect_equal(
+    yardstick:::factor_select(two_class_example, truth = truth, estimate = predicted),
+    list(truth = "truth", estimate = "predicted")
+ )
+  expect_equal(
+    yardstick:::factor_select(two_class_example, truth, predicted),
+    list(truth = "truth", estimate = "predicted")
+  )  
+  expect_equal(
+    yardstick:::factor_select(two_class_example,
+                              truth = truth,
+                              estimate = predicted,
+                              matches("Class")),
+    list(truth = "truth", estimate = "predicted")
+  )
+  expect_error(
+    yardstick:::factor_select(two_class_example,
+                              truth = truth,
+                              estimate = Class1,
+                              matches("Class"))
+  )
+  expect_error(
+    yardstick:::factor_select(two_class_example,
+                              truth = truth,
+                              estimate = matches("Class"))
+  )
+  expect_error(
+    yardstick:::factor_select(two_class_example, truth = tru)
+  )  
+})
+
+###################################################################
+
+test_that('just probs', {
+  expect_equal(
+    yardstick:::prob_select(two_class_example, truth = truth, matches("^Class")),
+    list(truth = "truth", probs = c("Class1", "Class2"))
+  )
+  expect_equal(
+    yardstick:::prob_select(two_class_example, truth = truth, Class1),
+    list(truth = "truth", probs = "Class1")
+  )  
+  expect_error(
+    yardstick:::prob_select(two_class_example,
+                            truth = truth,
+                            estimate = predicted,
+                            matches("^Class"))
+  )
+  expect_error(
+    yardstick:::prob_select(two_class_example,
+                            truth = truth,
+                            predicted)
+  )
+  expect_error(
+    yardstick:::prob_select(two_class_example,
+                            truth = Class1,
+                            Class2)
+  )
+  expect_error(
+    yardstick:::prob_select(two_class_example,
+                            truth = tru,
+                            Class2)
+  )  
+  expect_error(
+    yardstick:::prob_select(two_class_example,
+                            truth = truth,
+                            Class)
+  )   
+})
+
+###################################################################
+
+test_that('both', {
+  expect_equal(
+    yardstick:::both_select(two_class_example,
+                            truth = truth,
+                            estimate = predicted,
+                            Class1), 
+    list(truth = "truth", estimate = "predicted", probs = "Class1")
+  )
+  expect_equal(
+    yardstick:::both_select(two_class_example, truth, predicted, Class1, Class2), 
+    list(truth = "truth", estimate = "predicted", probs = c("Class1", "Class2"))
+  )  
+  expect_equal(
+    yardstick:::both_select(two_class_example,
+                              truth = truth,
+                              estimate = predicted,
+                              matches("Class")),
+    list(truth = "truth", estimate = "predicted", probs = c("Class1", "Class2"))
+  )
+  expect_error(
+    yardstick:::both_select(two_class_example,
+                              truth = truth,
+                              estimate = Class1)
+  )
+  expect_error(
+    yardstick:::both_select(two_class_example,
+                              truth = truth,
+                              estimate = matches("Class"))
+  )
+  expect_error(
+    yardstick:::both_select(two_class_example, truth = tru)
+  )  
+  expect_error(
+    yardstick:::both_select(two_class_example,
+                            truth = Class1,
+                            estimate = predicted,
+                            Class2)
+  )
+  expect_error(
+    yardstick:::both_select(two_class_example,
+                            truth = tru,
+                            estimate = predicted,
+                            Class2)
+  )  
+  expect_error(
+    yardstick:::both_select(two_class_example,
+                            truth = truth,
+                            estimate = predicted,
+                            Class)
+  )     
+})
