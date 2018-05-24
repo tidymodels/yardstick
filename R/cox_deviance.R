0e9cf356253e45e0cd795b8b32fec370728636e6
csetraynor <c.traynor@warwick.ac.uk>
2018-05-24 15:00:49 +0100

    checked bug coxdev

--- R/cox_deviance.R
+++ R/cox_deviance.R
@@ -11,21 +11,23 @@
 #' @seealso [coxph]
 #' @keywords deviance
 #' @examples
-#' data("surv_example")
-#'
-#' # Given a sample
 #' require(survival)
-#' mod <- coxph(Surv(time, status)~ age, data = surv_example)
+#' require(dplyr)
+#' data(lung)
+#' lung <- lung %>%
+#' mutate(status = (status == 2))
 #'
-#' get_deviance(surv_example, mod)
+#' mod <- coxph(Surv(time, status)~ age, data = lung)
+#'
+#' cox_deviance(mod)
 #' @author Carlos S Traynor
 #' @references
 #'
 #'  Terry M. Therneau and Patricia M. Grambsch (2000).
 #'   _Modeling Survival Data: Extending the Cox Model_.
 #'   Springer, New York. ISBN 0-387-98784-3.
-#' @export get_deviance
-get_deviance <-
+#' @export cox_deviance
+cox_deviance <-
   function( mod, ...) {
     -2*(mod$loglik)[2]
   }
