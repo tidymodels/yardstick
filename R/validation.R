d860a3160fb15b39fe454a7f827a531fd5dc13a7
DavisVaughan <davis@rstudio.com>
2018-10-30 14:32:12 -0400

    Export validate_estimator()

--- R/validation.R
+++ R/validation.R
@@ -190,6 +190,11 @@ validate_not_missing <- function(x, nm) {
 
 # Validate estimator type is allowed -------------------------------------------
 
+#' @param estimator_override A character vector overriding the default allowed
+#' estimator list of `c("binary", "macro", "micro", "macro_weighted")`. Set
+#' this is your classification estimator does not support all of these methods.
+#' @rdname developer-helpers
+#' @export
 validate_estimator <- function(estimator, estimator_override = NULL) {
 
   if(is.null(estimator)) {
