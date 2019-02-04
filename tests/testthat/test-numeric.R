bf602476bae86b35a28ab64edb5860186be147b8
DavisVaughan <davis@rstudio.com>
2019-02-04 11:40:49 -0500

    Add more validation tests.

--- tests/testthat/test-numeric.R
+++ tests/testthat/test-numeric.R
@@ -256,15 +256,67 @@ test_that('Pseudo-Huber Loss', {
 
 test_that('Mean Absolute Scaled Error', {
 
-  s <- data.frame(list(y = c(5,4,3,2,1),
-                       y_hat = c(1, 1, 1, 1, 1)))
+  truth <- ex_dat$obs
+  pred  <- ex_dat$pred
+
+  truth_lag <- dplyr::lag(truth, 1L)
+  naive_error <- truth - truth_lag
+  mae_denom <- mean(abs(naive_error)[-1])
+  scaled_error <- (truth - pred) / mae_denom
+  known_mase <- mean(abs(scaled_error))
+
+  m <- 2
+
+  truth_lag <- dplyr::lag(truth, m)
+  naive_error <- truth - truth_lag
+  mae_denom <- mean(abs(naive_error)[-c(1, 2)])
+  scaled_error <- (truth - pred) / mae_denom
+  known_mase_with_m <- mean(abs(scaled_error))
+
+  mae_train <- .5
+
+  mae_denom <- mae_train
+  scaled_error <- (truth - pred) / mae_denom
+  known_mase_with_mae_train <- mean(abs(scaled_error))
 
   expect_equal(
-    mase(s, truth = y, estimate = y_hat, m = 1L)[[".estimate"]],  2
+    mase(ex_dat, obs, pred)[[".estimate"]],
+    known_mase
+  )
+
+  expect_equal(
+    mase(ex_dat, obs, pred, m = 2)[[".estimate"]],
+    known_mase_with_m
+  )
+
+  expect_equal(
+    mase(ex_dat, obs, pred, mae_train = mae_train)[[".estimate"]],
+    known_mase_with_mae_train
   )
 
   expect_error(
-    mase(s, truth = y, estimate = y_hat, m = "should be int"),
-    "`m` must be a single integer value."
+    mase_vec(truth, pred, m = "x"),
+    "`m` must be a single positive integer value."
   )
+
+  expect_error(
+    mase_vec(truth, pred, m = -1),
+    "`m` must be a single positive integer value."
+  )
+
+  expect_error(
+    mase_vec(truth, pred, m = 1.5),
+    "`m` must be a single positive integer value."
+  )
+
+  expect_error(
+    mase_vec(truth, pred, mae_train = -1),
+    "`mae_train` must be a single positive numeric value."
+  )
+
+  expect_error(
+    mase_vec(truth, pred, mae_train = "x"),
+    "`mae_train` must be a single positive numeric value."
+  )
+
 })
