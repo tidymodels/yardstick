fbb90e02ec8235e9909cc3a187dd2072215e7905
DavisVaughan <davis@rstudio.com>
2019-07-30 09:49:42 -0400

    Tweak binary data docs

--- man-roxygen/examples-binary-data.R
+++ man-roxygen/examples-binary-data.R
@@ -1,7 +1,9 @@
 #' @examples
-#' # Two class example:
+#' # ---------------------------------------------------------------------------
+#' # Two class example
+#'
 #' # `truth` is a 2 level factor. The first level is `"Class1"`, which is the
 #' # "event of interest" by default in yardstick. See the Relevant Level
 #' # section above.
-#' data("two_class_example")
+#' data(two_class_example)
 #'
