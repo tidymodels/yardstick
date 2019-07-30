404a4963d4217ea70861996992ba1c9cab8fb623
DavisVaughan <davis@rstudio.com>
2019-07-30 10:12:52 -0400

    Small tweak to Relevant Level section

--- man-roxygen/event_first.R
+++ man-roxygen/event_first.R
@@ -5,7 +5,7 @@
 #' In `yardstick`, the default is to use the _first_ level. To
 #' change this, a global option called `yardstick.event_first` is
 #' set to `TRUE` when the package is loaded. This can be changed
-#' to `FALSE` if the last level of the factor is considered the
+#' to `FALSE` if the _last_ level of the factor is considered the
 #' level of interest by running: `options(yardstick.event_first = FALSE)`.
 #' For multiclass extensions involving one-vs-all
 #' comparisons (such as macro averaging), this option is ignored and
