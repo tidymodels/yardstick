a047b8b02b831971e2f204f684cc6485aff0db0c
DavisVaughan <davis@rstudio.com>
2019-08-02 16:44:59 -0400

    Add tests for undefined precision handling

--- tests/testthat/test-class-precision.R
+++ tests/testthat/test-class-precision.R
@@ -23,16 +23,52 @@ test_that('Two class - Powers paper', {
 
 # ------------------------------------------------------------------------------
 
-test_that("'micro' `NA` case is handled correctly", {
+test_that("Binary `precision()` returns `NA` with a warning when undefined (tp + fp = 0) (#98)", {
+  truth <- factor("a", levels = c("a", "b"))
+  estimate <- factor("b", levels = c("a", "b"))
 
-  truth <- factor(c(rep("a", 2), rep("b", 2)))
-  estimate <- factor(rep("b", length(truth)), levels(truth))
-
-  expect_equal(
-    precision_vec(truth, estimate, estimator = "micro"),
-    NA_real_
+  expect_warning(
+    expect_equal(
+      precision_vec(truth, estimate),
+      NA_real_
+    )
   )
 
+  cnd <- rlang::catch_cnd(precision_vec(truth, estimate))
+  expect_known_output(cat(cnd$message), "test-class-precision-warning-binary", print = TRUE)
+  expect_s3_class(cnd, "yardstick_warning_precision_undefined_binary")
+})
+
+test_that("Multiclass `precision()` returns averaged value with `NA`s removed + a warning when undefined (tp + fp = 0) (#98)", {
+  levels <- c("a", "b", "c", "d")
+
+  # When `d` is the event we get precision = 0 = 0 / (0 + 3)
+  # When `a` is the event we get a warning
+  # When `b` is the event we get a warning
+  # When `c` is the event we get a warning
+  truth <- factor(c("a", "b", "c"), levels = levels)
+  estimate <- factor(rep("d", 3), levels)
+  expect_warning(expect_equal(precision_vec(truth, estimate), 0))
+
+  # When `d` is the event we get precision = 0
+  # When `a` is the event we get precision = 1
+  # When `b` is the event we get precision = 0
+  # When `c` is the event we get a warning
+  truth <- factor(c("a", "b", "c"), levels = levels)
+  estimate <- factor(c("a", "d", "b"), levels)
+  expect_warning(expect_equal(precision_vec(truth, estimate), 1/3))
+
+  cnd <- rlang::catch_cnd(precision_vec(truth, estimate))
+  expect_known_output(cat(cnd$message), "test-class-precision-warning-multiclass", print = TRUE)
+  expect_s3_class(cnd, "yardstick_warning_precision_undefined_multiclass")
+})
+
+test_that("`NA` is still returned if there are some undefined precision values but `na.rm = FALSE`", {
+  levels <- c("a", "b", "c", "d")
+  truth <- factor(c("a", "b", "c"), levels = levels)
+  estimate <- factor(c("a", NA, "c"), levels)
+  expect_equal(precision_vec(truth, estimate, na_rm = FALSE), NA_real_)
+  expect_warning(precision_vec(truth, estimate, na_rm = FALSE), NA)
 })
 
 # sklearn compare --------------------------------------------------------------
