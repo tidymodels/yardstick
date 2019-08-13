816d71248ade4b89b379755635a612a26f4dcfa8
DavisVaughan <davis@rstudio.com>
2019-08-13 12:15:27 -0400

    Remove warning when outliers are symmetric, as the software doesn't warn either

--- R/num-iic.R
+++ R/num-iic.R
@@ -71,13 +71,6 @@ iic_vec <- function(truth, estimate, na_rm = TRUE, ...) {
     mae_neg <- mean(abs(delta_neg))
     mae_pos <- mean(abs(delta_pos))
 
-    delta_strictly_pos <- delta_pos[delta_pos > 0]
-    mae_strictly_pos <- mean(abs(delta_strictly_pos))
-
-    if (isTRUE(mae_neg == mae_strictly_pos)) {
-      warning("Index of ideality of correlation is useless when outliers are symmetric.")
-    }
-
     adjustment <- min(mae_neg, mae_pos) / max(mae_neg, mae_pos)
 
     try_cor(truth, estimate) * adjustment
