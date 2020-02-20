05ec3cb91971ef5e63af498e66ee9bf3a4b59d51
DavisVaughan <davis@rstudio.com>
2020-02-20 15:52:20 -0500

    Strip down tests to only what is required
    Also localizes `soybeans` and `measures_mlr` to only the test that requires it

--- tests/testthat/test-prob-roc_aunp.R
+++ tests/testthat/test-prob-roc_aunp.R
@@ -1,15 +1,8 @@
 context("AUNP")
 
-library(dplyr)
-
-# HPC_CV takes too long
-hpc_cv2 <- filter(hpc_cv, Resample %in% c("Fold06", "Fold07", "Fold08", "Fold09", "Fold10"))
-
-soybeans <- data_soybean()
-
-measures_mlr <- 0.964025841424236
-
 test_that("AUNP is equivalent to macro_weighted estimator", {
+  hpc_f1 <- data_hpc_fold1()
+
   expect_equal(
     roc_auc(hpc_f1, obs, VF:L, estimator = "macro_weighted")[[".estimate"]],
     roc_aunp(hpc_f1, obs, VF:L)[[".estimate"]]
@@ -24,6 +17,11 @@ test_that("AUNP errors on binary case", {
 })
 
 test_that("AUNP results match mlr for soybean example", {
+  soybeans <- data_soybean()
+
+  # Code to generate this value and `data_soybean()` is in `helper-data.R`
+  measures_mlr <- 0.964025841424236
+
   expect_equal(
     roc_aunp(soybeans, truth, `2-4-d-injury`:`rhizoctonia-root-rot`)[[".estimate"]],
     measures_mlr
--- tests/testthat/test-prob-roc_aunu.R
+++ tests/testthat/test-prob-roc_aunu.R
@@ -1,15 +1,8 @@
 context("AUNU")
 
-library(dplyr)
-
-# HPC_CV takes too long
-hpc_cv2 <- filter(hpc_cv, Resample %in% c("Fold06", "Fold07", "Fold08", "Fold09", "Fold10"))
-
-soybeans <- data_soybean()
-
-measures_mlr <- 0.963473055084008
-
 test_that("AUNU is equivalent to macro estimator", {
+  hpc_f1 <- data_hpc_fold1()
+
   expect_equal(
     roc_auc(hpc_f1, obs, VF:L, estimator = "macro")[[".estimate"]],
     roc_aunu(hpc_f1, obs, VF:L)[[".estimate"]]
@@ -24,6 +17,11 @@ test_that("AUNU errors on binary case", {
 })
 
 test_that("AUNU results match mlr for soybean example", {
+  soybeans <- data_soybean()
+
+  # Code to generate this value and `data_soybean()` is in `helper-data.R`
+  measures_mlr <- 0.963473055084008
+
   expect_equal(
     roc_aunu(soybeans, truth, `2-4-d-injury`:`rhizoctonia-root-rot`)[[".estimate"]],
     measures_mlr
