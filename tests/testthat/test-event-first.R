5f2e3644c241294d63abd2148dd927d43dc065d9
DavisVaughan <davis@rstudio.com>
2020-07-07 13:06:31 -0400

    Typo `binary-last` -> `binary_last`

--- tests/testthat/test-event-first.R
+++ tests/testthat/test-event-first.R
@@ -1,17 +1,17 @@
 test_that("`is_event_first()` works when ignoring the option", {
   expect_true(is_event_first("binary", check_option = FALSE))
-  expect_false(is_event_first("binary-last", check_option = FALSE))
+  expect_false(is_event_first("binary_last", check_option = FALSE))
 })
 
 test_that("multiclass `estimator` is not allowed", {
   expect_error(is_event_first("macro"), "Multiclass estimator")
 })
 
-test_that("`'binary-last'` overrides the option", {
+test_that("`'binary_last'` overrides the option", {
   rlang::local_options(yardstick.event_first = TRUE)
 
   expect_warning(
-    expect_false(is_event_first("binary-last")),
+    expect_false(is_event_first("binary_last")),
     "The `yardstick.event_first` option has been deprecated"
   )
 })
