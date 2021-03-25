04f6c60655eeb0313e80ba2bc0ca743f2d811865
DavisVaughan <davis@rstudio.com>
2021-03-25 15:39:50 -0400

    Allow `conf_mat()` to work with `class_pred` objects

--- tests/testthat/test-probably.R
+++ tests/testthat/test-probably.R
@@ -20,3 +20,20 @@ test_that("`class_pred` can be converted to `factor` when computing metrics", {
     NA_real_
   )
 })
+
+test_that("`class_pred` can be converted to `factor` with `conf_mat()`", {
+  skip_if_not_installed("probably")
+
+  cp_hpc_cv <- hpc_cv
+  cp_hpc_cv$obs <- probably::as_class_pred(cp_hpc_cv$obs, which = 1)
+  cp_hpc_cv$pred <- probably::as_class_pred(cp_hpc_cv$pred, which = 2)
+
+  fct_hpc_cv <- hpc_cv
+  fct_hpc_cv$obs[1] <- NA
+  fct_hpc_cv$pred[2] <- NA
+
+  expect_identical(
+    conf_mat(cp_hpc_cv, obs, pred),
+    conf_mat(fct_hpc_cv, obs, pred)
+  )
+})
