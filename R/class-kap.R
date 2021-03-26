a77549d31b0ee5f255e5124debe3eea05f2943ea
DavisVaughan <davis@rstudio.com>
2021-03-26 11:19:02 -0400

    Add `weighting` argument to `kap()`

--- R/class-kap.R
+++ R/class-kap.R
@@ -15,10 +15,26 @@
 #'
 #' @inheritParams sens
 #'
-#' @author Max Kuhn
+#' @param weighting A weighting to apply when computing the scores. One of:
+#'   `"none"`, `"linear"`, or `"quadratic"`. Linear and quadratic weighting
+#'   penalizes mis-predictions that are "far away" from the true value. Note
+#'   that distance is judged based on the ordering of the levels in `truth` and
+#'   `estimate`. It is recommended to provide ordered factors for `truth` and
+#'   `estimate` to explicitly code the ordering, but this is not required.
 #'
-#' @references Cohen, J. (1960). "A coefficient of agreement for nominal
-#'  scales". _Educational and Psychological Measurement_. 20 (1): 37-46.
+#'   In the binary case, all 3 weightings produce the same value, since it is
+#'   only ever possible to be 1 unit away from the true value.
+#'
+#' @author Max Kuhn
+#' @author Jon Harmon
+#'
+#' @references
+#'   Cohen, J. (1960). "A coefficient of agreement for nominal
+#'   scales". _Educational and Psychological Measurement_. 20 (1): 37-46.
+#'
+#'   Cohen, J. (1968). "Weighted kappa: Nominal scale agreement provision for
+#'   scaled disagreement or partial credit". _Psychological
+#'   Bulletin_. 70 (4): 213-220.
 #'
 #' @export
 #' @examples
@@ -49,8 +65,12 @@ kap <- new_class_metric(
 
 #' @export
 #' @rdname kap
-kap.data.frame  <- function(data, truth, estimate,
-                            na_rm = TRUE, ...) {
+kap.data.frame  <- function(data,
+                            truth,
+                            estimate,
+                            weighting = "none",
+                            na_rm = TRUE,
+                            ...) {
 
   metric_summarizer(
     metric_nm = "kap",
@@ -58,42 +78,48 @@ kap.data.frame  <- function(data, truth, estimate,
     data = data,
     truth = !!enquo(truth),
     estimate = !!enquo(estimate),
-    na_rm = na_rm
+    na_rm = na_rm,
+    metric_fn_options = list(weighting = weighting)
   )
 
 }
 
 #' @export
-kap.table <- function(data, ...) {
+kap.table <- function(data,
+                      weighting = "none",
+                      ...) {
   check_table(data)
   metric_tibbler(
     .metric = "kap",
     .estimator = finalize_estimator(data, metric_class = "kap"),
-    .estimate = kap_table_impl(data)
+    .estimate = kap_table_impl(data, weighting = weighting)
   )
 }
 
 #' @export
-kap.matrix <- function(data, ...) {
+kap.matrix <- function(data,
+                       weighting = "none",
+                       ...) {
   data <- as.table(data)
-  kap.table(data)
+  kap.table(data, weighting = weighting)
 }
 
 #' @export
 #' @rdname kap
-kap_vec <- function(truth, estimate, na_rm = TRUE, ...) {
-
+kap_vec <- function(truth,
+                    estimate,
+                    weighting = "none",
+                    na_rm = TRUE,
+                    ...) {
   estimator <- finalize_estimator(truth, metric_class = "kap")
 
-  kap_impl <- function(truth, estimate) {
-
+  kap_impl <- function(truth, estimate, weighting) {
     xtab <- vec2table(
       truth = truth,
       estimate = estimate
     )
 
-    kap_table_impl(xtab)
-
+    kap_table_impl(xtab, weighting = weighting)
   }
 
   metric_vec_template(
@@ -102,25 +128,74 @@ kap_vec <- function(truth, estimate, na_rm = TRUE, ...) {
     estimate = estimate,
     na_rm = na_rm,
     estimator = estimator,
-    cls = "factor"
+    cls = "factor",
+    weighting = weighting
   )
-
 }
 
-kap_table_impl <- function(data) {
-  kap_binary(data)
+kap_table_impl <- function(data, weighting) {
+  full_sum <- sum(data)
+  row_sum <- rowSums(data)
+  col_sum <- colSums(data)
+  expected <- outer(row_sum, col_sum) / full_sum
+
+  n_levels <- nrow(data)
+  w <- make_weighting_matrix(weighting, n_levels)
+
+  n_disagree <- sum(w * data)
+  n_chance <- sum(w * expected)
+
+  1 - n_disagree / n_chance
 }
 
-kap_binary <- function(data) {
+make_weighting_matrix <- function(weighting, n_levels) {
+  validate_weighting(weighting)
 
-  n <- sum(data)
+  if (is_no_weighting(weighting)) {
+    # [n_levels x n_levels], 0 on diagonal, 1 on off-diagonal
+    w <- matrix(1L, nrow = n_levels, ncol = n_levels)
+    diag(w) <- 0L
+    return(w)
+  }
 
-  .row_sums <- rowSums(data)
-  .col_sums <- colSums(data)
+  if (is_linear_weighting(weighting)) {
+    power <- 1L
+  } else {
+    # quadratic
+    power <- 2L
+  }
 
-  expected_acc <- sum( (.row_sums * .col_sums) / n ) / n
+  # [n_levels x n_levels], 0 on diagonal, increasing weighting on off-diagonal
+  w <- rlang::seq2(0L, n_levels - 1L)
+  w <- matrix(w, nrow = n_levels, ncol = n_levels)
+  w <- abs(w - t(w)) ^ power
 
-  obs_acc <- accuracy_binary(data)
+  w
+}
 
-  (obs_acc - expected_acc) / (1 - expected_acc)
+# ------------------------------------------------------------------------------
+
+validate_weighting <- function(x) {
+  if (!rlang::is_string(x)) {
+    abort("`weighting` must be a string.")
+  }
+
+  ok <- is_no_weighting(x) ||
+    is_linear_weighting(x) ||
+    is_quadratic_weighting(x)
+
+  if (!ok) {
+    abort("`weighting` must be 'none', 'linear', or 'quadratic'.")
+  }
+
+  invisible(x)
+}
+is_no_weighting <- function(x) {
+  identical(x, "none")
+}
+is_linear_weighting <- function(x) {
+  identical(x, "linear")
+}
+is_quadratic_weighting <- function(x) {
+  identical(x, "quadratic")
 }
