0df8590c173ff9082b5465277cde7fd9a7458570
DavisVaughan <davis@rstudio.com>
2022-03-29 17:18:24 -0400

    Greatly simplify `read_pydata()` by making data relative to `testthat/`
    Now it actually works when running tests interactively!

--- tests/pycompare/generate-pycompare.R
+++ tests/pycompare/generate-pycompare.R
@@ -40,7 +40,7 @@ save_metric_results <- function(nm, fn, ..., average = c("macro", "micro", "weig
   res <- c(res, res2, list(case_weight))
   names(res) <- c("binary", average, "case_weight")
 
-  saveRDS(res, paste0("tests/pycompare/py-", nm), version = 2)
+  saveRDS(res, test_path("py-data", paste0("py-", nm, ".rds")), version = 2)
 }
 
 # ------------------------------------------------------------------------------
@@ -56,14 +56,14 @@ py_mcc <- list(
   binary = skmetrics$matthews_corrcoef(two_class_example$truth, two_class_example$predicted),
   multiclass = skmetrics$matthews_corrcoef(hpc_cv$obs, hpc_cv$pred)
 )
-saveRDS(py_mcc, "tests/pycompare/py-mcc", version = 2)
+saveRDS(py_mcc, test_path("py-data", "py-mcc.rds"), version = 2)
 
 # Accuracy
 py_accuracy <- list(
   binary = skmetrics$accuracy_score(two_class_example$truth, two_class_example$predicted),
   multiclass = skmetrics$accuracy_score(hpc_cv$obs, hpc_cv$pred)
 )
-saveRDS(py_accuracy, "tests/pycompare/py-accuracy", version = 2)
+saveRDS(py_accuracy, test_path("py-data", "py-accuracy.rds"), version = 2)
 
 # Kappa
 py_kap <- list(
@@ -74,7 +74,7 @@ py_kap <- list(
   quadratic_binary = skmetrics$cohen_kappa_score(two_class_example$truth, two_class_example$predicted, levels(two_class_example$truth), weights = "quadratic"),
   quadratic_multiclass = skmetrics$cohen_kappa_score(hpc_cv$obs, hpc_cv$pred, labels = levels(hpc_cv$obs), weights = "quadratic")
 )
-saveRDS(py_kap, "tests/pycompare/py-kap", version = 2)
+saveRDS(py_kap, test_path("py-data", "py-kap.rds"), version = 2)
 
 # RMSE
 py_rmse <- list(
@@ -84,7 +84,7 @@ py_rmse <- list(
     sample_weight = weights_solubility_test
   ))
 )
-saveRDS(py_rmse, "tests/pycompare/py-rmse", version = 2)
+saveRDS(py_rmse, test_path("py-data", "py-rmse.rds"), version = 2)
 
 # Balanced Accuracy
 # Not comparing multiclass against sklearn here, because they use a different definition
@@ -97,4 +97,4 @@ py_bal_accuracy <- list(
     )
   )
 )
-saveRDS(py_bal_accuracy, "tests/pycompare/py-bal-accuracy", version = 2)
+saveRDS(py_bal_accuracy, test_path("py-data", "py-bal-accuracy.rds"), version = 2)
--- tests/testthat/helper-read_pydata.R
+++ tests/testthat/helper-read_pydata.R
@@ -1,15 +1,5 @@
 read_pydata <- function(py_path) {
-  # depending on if testing one at a time or running
-  # CTRL+SHIFT+T
-  if(interactive()) {
-    # interactive + local use. devtools::test() interactively
-    if (Sys.getenv("NOT_CRAN") == "true") {
-      path <- "../pycompare/"
-    } else {
-      path <- "tests/pycompare/"
-    }
-  } else {
-    path <- "../pycompare/"
-  }
-  readRDS(paste0(path, py_path))
+  py_path <- paste0(py_path, ".rds")
+  py_path <- test_path("py-data", py_path)
+  readRDS(py_path)
 }
