419cc694447f315fb9df70fb9d4b547f4e07c76e
DavisVaughan <davis@rstudio.com>
2022-04-18 11:05:06 -0400

    Remove unused prob helpers

--- tests/testthat/helper-macro-prob.R
+++ tests/testthat/helper-macro-prob.R
@@ -40,22 +40,3 @@ hpc_fold1_macro_weighted_metric <- function(binary_metric, ...) {
 
   stats::weighted.mean(res, macro_wt)
 }
-
-# TODO: Remove this and all below when all prob metrics support case weights
-# These get embedded in the helper functions below
-hpc_f1 <- data_hpc_fold1()
-wt <- as.vector(table(hpc_f1$obs))
-macro_wt <- wt / sum(wt)
-truth <- hpc_f1$obs
-prob_mat <- as.matrix(dplyr::select(hpc_f1, VF:L))
-
-# Just pass in a binary metric function
-prob_macro_metric <- function(binary_metric, ...) {
-  res <- rlang::flatten_dbl(one_vs_all_impl(binary_metric, truth, prob_mat, ...))
-  mean(res)
-}
-
-prob_macro_weighted_metric <- function(binary_metric, ...) {
-  res <- rlang::flatten_dbl(one_vs_all_impl(binary_metric, truth, prob_mat, ...))
-  stats::weighted.mean(res, macro_wt)
-}
