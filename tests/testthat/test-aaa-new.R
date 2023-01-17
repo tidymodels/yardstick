8ddf7baf42c2449bcfe52103b655216ef8d06aad
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-01-17 15:07:29 -0800

    add tests for new_dynamic_survival_metric and new_static_survival_metric

--- tests/testthat/test-aaa-new.R
+++ tests/testthat/test-aaa-new.R
@@ -3,16 +3,22 @@ test_that("can create metric functions", {
   fn2 <- new_prob_metric(function() 1, "maximize")
   fn3 <- new_numeric_metric(function() 1, "minimize")
   fn3zero <- new_numeric_metric(function() 1, "zero")
+  fn4 <- new_dynamic_survival_metric(function() 1, "minimize")
+  fn5 <- new_static_survival_metric(function() 1, "minimize")
 
   expect_identical(class(fn1), c("class_metric", "metric", "function"))
   expect_identical(class(fn2), c("prob_metric", "metric", "function"))
   expect_identical(class(fn3), c("numeric_metric", "metric", "function"))
   expect_identical(class(fn3zero), c("numeric_metric", "metric", "function"))
+  expect_identical(class(fn4), c("dynamic_survival_metric", "metric", "function"))
+  expect_identical(class(fn5), c("static_survival_metric", "metric", "function"))
 
   expect_identical(attr(fn1, "direction"), "maximize")
   expect_identical(attr(fn2, "direction"), "maximize")
   expect_identical(attr(fn3, "direction"), "minimize")
   expect_identical(attr(fn3zero, "direction"), "zero")
+  expect_identical(attr(fn4, "direction"), "minimize")
+  expect_identical(attr(fn5, "direction"), "minimize")
 })
 
 test_that("`fn` is validated", {
