019717bbe1200d9203268356b27799823571c3e6
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-01-25 10:46:17 -0800

    skip concordance survival tests for now

--- tests/testthat/test-surv-concordance.R
+++ tests/testthat/test-surv-concordance.R
@@ -1,4 +1,5 @@
 test_that("comparison test with survival", {
+  skip("waiting for concordance rewrite")
   expected_res <- concordance_survival(
     data = lung_surv, truth = surv_obj, estimate = age
   )
@@ -10,6 +11,7 @@ test_that("comparison test with survival", {
 })
 
 test_that("case weights works", {
+  skip("waiting for concordance rewrite")
   expected_res <- concordance_survival(
     data = lung_surv, truth = surv_obj, estimate = age, case_weights = ph.ecog
   )
