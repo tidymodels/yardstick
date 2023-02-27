1414a7aaacb34eab5f8a8720db61ff3bfe62a86e
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-02-27 14:31:14 -0800

    add return-dynamic-survival.R

---
+++ man-roxygen/return-dynamic-survival.R
@@ -0,0 +1,10 @@
+#' @return
+#'
+#' A `tibble` with columns `.metric`, `.estimator`,
+#' and `.estimate` and 1 row of values.
+#'
+#' For grouped data frames, the number of rows returned will be the same as
+#' the number of groups.
+#'
+#' For `<%=fn %>_vec()`, a `numeric` vector same length as the input argument
+#' `eval_time`. (or `NA`).
