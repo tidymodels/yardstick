b796c2653b2e964211b78522a3c2145c6becedff
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-03-13 16:32:50 -0700

    add tests for concordance_survival()

---
+++ tests/testthat/test-surv-concordance.R
@@ -0,0 +1,21 @@
+test_that("comparison test with survival", {
+  expected_res <- concordance_survival(
+    data = lung_surv, truth = surv_obj, estimate = age
+  )
+
+  expect_equal(
+    expected_res[[".estimate"]],
+    survival::concordance(surv_obj ~ age, data = lung_surv)$concordance
+  )
+})
+
+test_that("case weights works", {
+  expected_res <- concordance_survival(
+    data = lung_surv, truth = surv_obj, estimate = age, case_weights = ph.ecog
+  )
+
+  expect_equal(
+    expected_res[[".estimate"]],
+    survival::concordance(surv_obj ~ age, weights = ph.ecog, data = lung_surv)$concordance
+  )
+})
