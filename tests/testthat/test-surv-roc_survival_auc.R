b0e0e23d1c4a94a4a7572175113ea34d02263b16
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-04-07 20:24:05 -0700

    add riskRegression comparison to roc_auc_survival()

--- tests/testthat/test-surv-roc_survival_auc.R
+++ tests/testthat/test-surv-roc_survival_auc.R
@@ -20,3 +20,35 @@ test_that("roc_curve_auc() calculations", {
     survival_auc$.estimate
   )
 })
+
+# riskRegression compare -------------------------------------------------------
+
+test_that('riskRegression equivalent', {
+  riskRegression_res <- readRDS(test_path("data/auc_churn_res.rds"))
+
+  yardstick_res <- readRDS(test_path("data/rr_churn_data.rds")) %>%
+    dplyr::rename(
+      .eval_time = times,
+      .pred_survival = surv_prob,
+      .weight_censored = ipcw
+    ) %>%
+    dplyr::mutate(
+      .weight_censored = dplyr::if_else(status == 0 & time < .eval_time, NA, .weight_censored)
+    ) %>%
+    tidyr::nest(.pred = -c(ID, time, status, model)) %>%
+    dplyr::mutate(surv_obj = survival::Surv(time, status)) %>%
+    roc_auc_survival(
+      truth = surv_obj,
+      .pred
+    )
+
+  expect_identical(
+    riskRegression_res$times,
+    yardstick_res$.eval_time
+  )
+
+  expect_equal(
+    riskRegression_res$Brier,
+    yardstick_res$.estimate
+  )
+})
