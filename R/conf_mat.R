7f2a3a19c83d5f25f31631ff5c64a158056b17a1
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-04-18 12:29:11 -0700

    use as_factor_from_class_pred() in conf_mat()

--- R/conf_mat.R
+++ R/conf_mat.R
@@ -216,6 +216,13 @@ conf_mat.grouped_df <- function(data,
 }
 
 conf_mat_impl <- function(truth, estimate, case_weights, call = caller_env()) {
+  if (is_class_pred(truth)) {
+    truth <- as_factor_from_class_pred(truth)
+  }
+  if (is_class_pred(estimate)) {
+    estimate <- as_factor_from_class_pred(estimate)
+  }
+
   estimator <- "not binary"
   check_class_metric(truth, estimate, case_weights, estimator, call = call)
 
