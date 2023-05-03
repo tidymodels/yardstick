64c896634a888fa6e1e81b57b46d82dbf340e3ae
simonpcouch <simonpatrickcouch@gmail.com>
2023-05-03 09:39:44 -0400

    revert 6334391
    some settings actually result in `set_known_selection()` passing `"init"`.

--- R/template.R
+++ R/template.R
@@ -758,7 +758,6 @@ yardstick_eval_select <- function(expr,
                                   ...,
                                   error_call = caller_env()) {
   check_dots_empty(call = error_call)
-  expr_name <- "init"
 
   if (!quo_is_missing(expr) && inherits(quo_get_expr(expr), "name")) {
     expr_name <- as_name(expr)
@@ -783,7 +782,7 @@ yardstick_eval_select <- function(expr,
     abort(message, call = error_call)
   }
 
-  set_known_selection(expr_name, out)
+  set_known_selection(as_name(expr), out)
 
   out
 }
@@ -792,7 +791,6 @@ yardstick_eval_select_dots <- function(...,
                                        data,
                                        error_call = caller_env()) {
   expr <- quo(!!substitute(...))
-  expr_label <- "init"
 
   if (!quo_is_missing(expr) && inherits(quo_get_expr(expr), c("name", "call"))) {
     expr_label <- as_label(expr)
@@ -813,7 +811,7 @@ yardstick_eval_select_dots <- function(...,
 
   out <- names(out)
 
-  set_known_selection(expr_label, out)
+  set_known_selection(as_label(expr), out)
 
   out
 }
