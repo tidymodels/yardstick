c260656d15e2514750a3798b96512092f2674a5d
simonpcouch <simonpatrickcouch@gmail.com>
2023-05-15 08:46:56 -0400

    validate canonical metrics against fairlearn

---
+++ tests/testthat/test-fair-demographic_parity.R
@@ -0,0 +1,21 @@
+test_that("result matches reference implementation (fairlearn)", {
+  data("hpc_cv")
+  py_res <- read_pydata("py-demographic_parity")
+
+  hpc_cv$obs_vf = as.factor(hpc_cv$obs == "VF")
+  hpc_cv$pred_vf = as.factor(hpc_cv$pred == "VF")
+  hpc_cv$case_weights <- read_weights_hpc_cv()
+
+  dp <- demographic_parity(Resample)
+
+  expect_equal(
+    dp(hpc_cv, truth = obs_vf, estimate = pred_vf, event_level = "second")$.estimate,
+    py_res$binary
+  )
+
+  expect_equal(
+    dp(hpc_cv, truth = obs_vf, estimate = pred_vf,
+       event_level = "second", case_weights = case_weights)$.estimate,
+    py_res$weighted
+  )
+})
---
+++ tests/testthat/test-fair-equal_opportunity.R
@@ -0,0 +1,21 @@
+test_that("result matches reference implementation (fairlearn)", {
+  data("hpc_cv")
+  py_res <- read_pydata("py-equal_opportunity")
+
+  hpc_cv$obs_vf = as.factor(hpc_cv$obs == "VF")
+  hpc_cv$pred_vf = as.factor(hpc_cv$pred == "VF")
+  hpc_cv$case_weights <- read_weights_hpc_cv()
+
+  eo <- equal_opportunity(Resample)
+
+  expect_equal(
+    eo(hpc_cv, truth = obs_vf, estimate = pred_vf, event_level = "second")$.estimate,
+    py_res$binary
+  )
+
+  expect_equal(
+    eo(hpc_cv, truth = obs_vf, estimate = pred_vf,
+       event_level = "second", case_weights = case_weights)$.estimate,
+    py_res$weighted
+  )
+})
