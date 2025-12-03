87b5f36eff22f3228d7f2195cbb6c62527d74474
Daniel J. McDonald <dajmcdon@gmail.com>
2025-12-03 15:12:25 -0800

    fix NA handling

--- R/quant-weighted_interval.R
+++ R/quant-weighted_interval.R
@@ -11,17 +11,33 @@
 #' @param actual double. Actual value(s)
 #' @param quantile_levels probabilities. If specified, the score will be
 #'   computed at this set of levels. Otherwise, those present in `x` will be
-#'   used.
-#' @param na_handling character. Determines how missing values are handled.
-#'   For `"impute"`, missing values will be
-#'   calculated if possible using the available quantiles. For `"drop"`,
-#'   explicitly missing values are ignored in the calculation of the score, but
-#'   implicitly missing values are imputed if possible.
-#'   For `"propogate"`, the resulting score will be `NA` if any missing values
-#'   exist. Finally, if
-#'   `quantile_levels` is specified, `"fail"` will result in
-#'   the score being `NA` when any required quantile levels (implicit or explicit)
-#'   do not have corresponding values.
+#'   used. If `quantile_levels` do not exactly match those available in `x`,
+#'   then some quantiles will have implicit missingness. Handling of these
+#'   is determined by `quantile_estimate_nas`.
+#' @param quantile_estimate_nas character. This argument applies only to `x`.
+#'   It handles imputation of individual `quantile_levels` that are necessary to
+#'   compute a score. Because each element of `x` is a [hardhat::quantile_pred],
+#'   it is possible for these to be missing for particular
+#'   `quantile_levels`. There are a number of different possibilities for such
+#'   missingness. The options are as follows:
+#'   * For `"impute"`, both explicit and implicit missing values will be imputed
+#'   using [hardhat::impute_quantiles()] prior to the calculation of the score.
+#'   So the score will be `NA` only if imputation fails.
+#'   * For `"drop"`, any explicit missing values will be removed
+#'   before calculating the score for a particular prediction. This may be
+#'   reasonable due to the weighting. For example, if the estimate has
+#'   `quantile_levels = c(.25, .5, .75)` but the median is `NA` for a particular
+#'   prediction, it may be reasonable to average the accuracy of `c(.25, .75)`
+#'   for that prediction with others that don't have missingness. This option
+#'   is only works if `quantile_levels = NULL` or is a subset of the
+#'   `quantile_levels` in `x`.
+#'   * For `"propagate"`, any missing value predictions will result in that
+#'   element of `x` having a score of `NA`. If `na_rm = TRUE`, then these will
+#'   be removed before averaging.
+#' @param na_rm logical. If `TRUE`, missing values in `actual` or both implicit and
+#'   explicit (values of `NA` present in `x`), will be ignored (dropped) in the
+#'   calculation of the summary score. If `FALSE` (the default), any `NA`s will
+#'   result in the summary being `NA`.
 #' @param ... not used
 #'
 #' @return a vector of nonnegative scores.
@@ -29,26 +45,37 @@
 #' @export
 #' @examples
 #' quantile_levels <- c(.2, .4, .6, .8)
-#' predq1 <- 1:4 #
-#' predq2 <- 8:11
-#' dstn <- quantile_pred(rbind(predq1, predq2), quantile_levels)
-#' actual <- c(3.3, 7.1)
-#' weighted_interval_score(dstn, actual)
-#' weighted_interval_score(dstn, actual, c(.25, .5, .75))
+#' pred1 <- 1:4
+#' pred2 <- 8:11
+#' preds <- quantile_pred(rbind(pred1, pred2), quantile_levels)
+#' truth <- c(3.3, 7.1)
+#' weighted_interval_score_vec(truth, preds)
+#' weighted_interval_score_vec(truth, preds, quantile_levels = c(.25, .5, .75))
 #'
 #' # Missing value behaviours
-#' dstn <- quantile_pred(matrix(c(1, 2, NA, 4), nrow = 1), 1:4 / 5)
-#' weighted_interval_score(dstn, 2.5)
-#' weighted_interval_score(dstn, 2.5, 1:9 / 10)
-#' weighted_interval_score(dstn, 2.5, 1:9 / 10, na_handling = "drop")
-#' weighted_interval_score(dstn, 2.5, na_handling = "propagate")
-#' weighted_interval_score(
-#'   quantile_pred(matrix(1:4, nrow = 1), 1:4 / 5),
-#'   actual = 2.5,
-#'   quantile_levels = 1:9 / 10,
-#'   na_handling = "fail"
-#' )
 #'
+#' preds_na <- quantile_pred(rbind(pred1, c(1, 2, NA, 4)), 1:4 / 5)
+#' truth <- c(2.5, 2.5)
+#' weighted_interval_score_vec(truth, preds_na)
+#' weighted_interval_score_vec(truth, preds_na, quantile_levels = 1:9 / 10)
+#' expect_error(weighted_interval_score_vec(
+#'   truth,
+#'   preds_na,
+#'   quantile_levels = 1:9 / 10,
+#'   quantile_estimate_nas = "drop"
+#' ))
+#' weighted_interval_score_vec(
+#'   truth,
+#'   preds_na,
+#'   quantile_levels = c(2, 3) / 5,
+#'   quantile_estimate_nas = "drop"
+#' )
+#' weighted_interval_score_vec(
+#'   truth, preds_na, na_rm = TRUE, quantile_estimate_nas = "propagate"
+#' )
+#' weighted_interval_score_vec(
+#'   truth, preds_na, quantile_estimate_nas = "propagate"
+#' )
 #'
 weighted_interval_score <- function(data, ...) {
   UseMethod("weighted_interval_score")
@@ -64,78 +91,84 @@ weighted_interval_score_vec <- function(
   truth,
   estimate,
   quantile_levels = NULL,
-  na_rm = TRUE,
-  na_impute = c("none", "explicit", "any"),
+  na_rm = FALSE,
+  quantile_estimate_nas = c("impute", "drop", "propagate"),
   case_weights = NULL,
   ...
 ) {
   check_quantile_metric(truth, estimate, case_weights)
   estimate_quantile_levels <- hardhat::extract_quantile_levels(estimate)
-  na_impute <- rlang::arg_match(na_impute)
-
-  # If we requested particular levels, but we don't impute, and the levels
-  # aren't all there, then return NA
+  quantile_estimate_nas <- rlang::arg_match(quantile_estimate_nas)
   if (!is.null(quantile_levels)) {
     hardhat::check_quantile_levels(quantile_levels)
-    if (
-      na_impute == "none" &&
-        !(all(quantile_levels %in% estimate_quantile_levels))
-    ) {
+    all_levels_estimated <- all(quantile_levels %in% estimate_quantile_levels)
+    if (quantile_estimate_nas == "drop" && !all_levels_estimated) {
+      cli::cli_abort(
+        "When `quantile_levels` is not a subset of those available in `estimate`, 
+      `quantile_estimate_nas` may not be `'drop'`."
+      )
+    }
+    if (!all_levels_estimated && (quantile_estimate_nas == "propagate")) {
+      # We requested particular levels, but the levels aren't all there,
+      # and NAs propagate, so return NA
       return(NA_real_)
     }
   }
+
   quantile_levels <- quantile_levels %||% estimate_quantile_levels
-
-  if (na_rm) {
-    result <- yardstick_remove_missing(truth, estimate, case_weights)
-
-    truth <- result$truth
-    estimate <- result$estimate
-    case_weights <- result$case_weights
-  } else if (yardstick_any_missing(truth, estimate, case_weights)) {
-    return(NA_real_)
+  if (quantile_estimate_nas %in% c("drop", "propagate")) {
+    levels_estimated <- estimate_quantile_levels %in% quantile_levels
+    estimate <- as.matrix(estimate)[, levels_estimated, drop = FALSE]
+  } else {
+    estimate <- as.matrix(hardhat::impute_quantiles(estimate, quantile_levels))
   }
 
-  wis_impl(
+  vec_wis <- wis_impl(
     truth = truth,
     estimate = estimate,
     quantile_levels = quantile_levels,
-    na_impute = na_impute,
-    case_weights = case_weights
+    rowwise_na_rm = (quantile_estimate_nas == "drop")
   )
+
+  if (na_rm) {
+    result <- yardstick_remove_missing(truth, vec_wis, case_weights)
+
+    truth <- result$truth
+    vec_wis <- result$estimate
+    case_weights <- result$case_weights
+  } else if (yardstick_any_missing(truth, vec_wis, case_weights)) {
+    return(NA_real_)
+  }
+
+  yardstick_mean(vec_wis, case_weights = case_weights)
 }
 
 wis_impl <- function(
   truth,
   estimate,
-  quantile_levels = NULL,
-  na_impute = c("none", "explicit", "any"),
-  case_weights = NULL,
-  ...
+  quantile_levels,
+  rowwise_na_rm = TRUE
 ) {
-  estimate <- impute_quantiles(
-    estimate,
-    quantile_levels,
-    replace_na = (na_handling == "impute")
-  )
-  estimate <- as.matrix(estimate)[,
-    attr(x, "quantile_levels") %in% quantile_levels,
-    drop = FALSE
-  ]
-  na_rm <- (na_handling == "drop")
-  errors <- as.vector(
+  as.vector(
     mapply(
-      FUN = function(.x, .y) wis_one_quantile(.x, quantile_levels, .y, na_rm),
+      FUN = function(.x, .y) {
+        wis_one_quantile(.x, quantile_levels, .y, rowwise_na_rm)
+      },
       vctrs::vec_chop(estimate),
       truth
     ),
     "double"
   )
-
-  yardstick_mean(vec_score, case_weights = case_weights)
 }
 
 
-wis_one_quantile <- function(q, tau, actual, na_rm) {
-  2 * mean(pmax(tau * (actual - q), (1 - tau) * (q - actual)), na.rm = na_rm)
+wis_one_quantile <- function(values, quantile_levels, truth, na_rm) {
+  2 *
+    mean(
+      pmax(
+        quantile_levels * (truth - values),
+        (1 - quantile_levels) * (values - truth)
+      ),
+      na.rm = na_rm
+    )
 }
